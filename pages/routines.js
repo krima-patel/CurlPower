93049ac17c8f6ad6342e58006b20a85476605ba3
Krima Patel <patel.krima@hotmail.com>
2022-08-29 19:16:45 -0500

    fixed user name and display

--- pages/routines.js
+++ pages/routines.js
@@ -1,18 +1,16 @@
 import { useState, useEffect } from 'react';
 import { getRoutines } from '../api/routineData';
-import { useAuth } from '../utils/context/authContext';
 import RoutineCard from '../components/RoutineCard';
 
 export default function Routines() {
   const [routines, setRoutines] = useState([]);
-  const { user } = useAuth();
   const getAllRoutines = () => {
-    getRoutines(user).then(setRoutines);
+    getRoutines().then(setRoutines);
   };
   useEffect(() => {
     getAllRoutines();
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user]);
+  }, []);
 
   return (
     <div className="d-flex flex-wrap">
